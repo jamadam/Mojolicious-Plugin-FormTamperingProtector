317fc0457767330f45e693598229b7ad381e8282
jamadam <sugama@jamadam.com>
2013-06-04 01:02:34 +0900

    initial commit

---
+++ lib/Mojolicious/Plugin/FormTamperingProtecter.pm
@@ -0,0 +1,127 @@
+package Mojolicious::Plugin::FormTamperingProtecter;
+use strict;
+use warnings;
+use Mojo::Base 'Mojolicious::Plugin';
+our $VERSION = '0.01';
+use Data::Dumper;
+use Mojo::JSON;
+use Mojo::Util qw{encode xml_escape hmac_sha1_sum secure_compare};
+
+    has 'secret';
+    
+    my $json = Mojo::JSON->new;
+    
+    ### ---
+    ### register
+    ### ---
+    sub register {
+        my ($self, $app, $options) = @_;
+        
+        $self->secret($app->secret);
+        
+        $app->hook('around_dispatch' => sub {
+            (my $app, my $c) = @_;
+            
+            my $token_key_prefix = $options->{token_key_prefix};
+            
+            my @actions =
+                ref $options->{action} ? @{$options->{action}} : $options->{action};
+            
+            if ($c->req->method eq 'POST' && grep($c->req->url->path, @actions)) {
+                my $token = $c->param("$token_key_prefix-token");
+                if (my $error = $self->tampered($c, $token)) {
+                    return $options->{blackhole}->($c, $error);
+                }
+            }
+            
+            $app->();
+            
+            my $dom = $c->res->dom;
+            
+            for my $action (@actions) {
+                $dom->find("form[action=$action]")->each(sub {
+                    my $form = shift;
+                    my $names = [];
+                    my $static = {};
+                    my $targets = $form->find("*[name]")->each(sub {
+                        my $tag = shift;
+                        my $name = $tag->attrs('name');
+                        push(@$names, $name);
+                        if ($tag->attrs('type') eq 'hidden') {
+                            $static->{$name} = $tag->attrs('value');
+                        }
+                    });
+                    my $digest = $self->sign($json->encode({names => $names, static => $static}));
+                    my $digest_html = xml_escape $digest;
+                    $form->append_content(
+                        qq!<input type="hidden" name="$token_key_prefix-token" value="$digest_html">!);
+                });
+            }
+            $c->res->body(encode('UTF-8', $dom));
+        });
+    }
+    
+    sub tampered {
+        my ($self, $c, $token) = @_;
+        if (! $token) {
+            return 'Token not found';
+        }
+        if (my $unsigned = $self->unsign($token)) {
+            my $digest = $json->decode($unsigned);
+            for my $name (@{$digest->{'names'}}) {
+                if (! defined($c->param($name))) {
+                    return "Form key $name not given";
+                }
+            }
+            for my $name (keys %{$digest->{'static'}}) {
+                if ($c->param($name) ne $digest->{'static'}->{$name}) {
+                    return "Hidden field $name has tampered";
+                }
+            }
+        } else {
+            return 'Token has tampered';
+        }
+        return;
+    }
+    
+    sub sign {
+        my ($self, $value) = @_;
+        return "$value--" . hmac_sha1_sum($value, $self->secret);
+    }
+    
+    sub unsign {
+        my ($self, $signed) = @_;
+        if ($signed =~ s/--([^\-]+)$//) {
+            my $sig = $1;
+            if (secure_compare($sig, hmac_sha1_sum($signed, $self->secret))) {
+                return $signed;
+            }
+        }
+    }
+
+1;
+
+__END__
+
+=head1 NAME
+
+Mojolicious::Plugin::FormTamperingProtecter - FormTamperingProtecter
+
+=head1 SYNOPSIS
+  
+=head1 DESCRIPTION
+
+=head2 OPTIONS
+
+=head1 AUTHOR
+
+Sugama Keita, E<lt>sugama@jamadam.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Sugama Keita.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
---
+++ t/02_basic.t
@@ -0,0 +1,107 @@
+package Template_Basic;
+use strict;
+use warnings;
+use utf8;
+use Test::Mojo;
+use Mojolicious::Lite;
+use Test::More tests => 26;
+
+my $token_key_prefix = 'fomr-tampering-protecter';
+
+plugin form_tampering_protecter => {
+	token_key_prefix => $token_key_prefix,
+	action => '/receptor1',
+	blackhole => sub {
+		$_[0]->res->code(400);
+		$_[0]->render(text => $_[1]);
+	},
+};
+
+get '/test1' => sub {
+  my $self = shift;
+  $self->render(text => <<EOF);
+<html>
+	<body>
+		<form action="/receptor1">
+			<input type="text" name="foo" value="fooValue">
+			<input type="text" name="bar" value="barValue">
+			<input type="hidden" name="baz" value="bazValue">
+		</form>
+		<span id="jp">やったー</span>
+	</body>
+</html>
+EOF
+};
+
+post '/receptor1' => sub {
+	shift->render(text => 'post completed');
+};
+
+my $t = Test::Mojo->new;
+my $dom;
+
+$t->get_ok('/test1');
+$t->status_is(200);
+my $token = $t->tx->res->dom->at("form input[name=$token_key_prefix-token]")->attrs('value');
+like $token, qr/\Q{"names":["foo","bar","baz"],"static":{"baz":"bazValue"}}\E--.+/;
+$t->text_is("#jp", 'やったー');
+
+$t->post_ok('/receptor1' => form => {
+	foo => 'fooValue',
+	bar => 'barValue',
+	baz => 'bazValue',
+	"$token_key_prefix-token" => $token,
+});
+$t->status_is(200);
+$t->content_is('post completed');
+
+$t->post_ok('/receptor1' => form => {
+	bar => 'barValue',
+	baz => 'bazValue',
+	"$token_key_prefix-token" => $token,
+});
+$t->status_is(400);
+$t->content_like(qr{foo});
+
+$t->post_ok('/receptor1' => form => {
+	foo => 'fooValue',
+	bar => 'barValue',
+	baz => 'bazValue-tampered!',
+	"$token_key_prefix-token" => $token,
+});
+$t->status_is(400);
+$t->content_like(qr{baz});
+$t->content_like(qr{tampered});
+
+$t->post_ok('/receptor1' => form => {
+	foo => 'fooValue',
+	bar => 'barValue',
+	baz => 'bazValue',
+});
+$t->status_is(400);
+$t->content_like(qr{Token});
+$t->content_like(qr{not found});
+
+$t->post_ok('/receptor1' => form => {
+	foo => 'fooValue',
+	bar => 'barValue',
+	baz => 'bazValue',
+	"$token_key_prefix-token" => $token.'-tampered',
+});
+$t->status_is(400);
+$t->content_like(qr{Token});
+$t->content_like(qr{tampered});
+
+$t->post_ok('/receptor1' => form => {
+	foo => 'fooValue',
+	bar => 'barValue',
+	baz => 'bazValue',
+	"$token_key_prefix-token" => 'tampered-'. $token,
+});
+$t->status_is(400);
+$t->content_like(qr{Token});
+$t->content_like(qr{tampered});
+
+1;
+
+__END__
