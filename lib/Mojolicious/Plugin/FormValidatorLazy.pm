1af902c0ef2f8b2189d9ed26e7504cfbaf6c4855
jamadam <sugama@jamadam.com>
2013-08-06 00:29:24 +0900

    made internal methods exportable

--- lib/Mojolicious/Plugin/FormValidatorLazy.pm
+++ lib/Mojolicious/Plugin/FormValidatorLazy.pm
@@ -7,8 +7,6 @@ use Data::Dumper;
 use Mojo::JSON;
 use Mojo::Util qw{encode xml_escape hmac_sha1_sum secure_compare};
 
-has 'secret';
-
 my $DIGEST_KEY_ALLOW_NULL = 0;
 my $DIGEST_KEY_MAXLENGTH  = 1;
 my $DIGEST_KEY_REQUIRED   = 2;
@@ -26,11 +24,10 @@ my $json = Mojo::JSON->new;
 sub register {
     my ($self, $app, $options) = @_;
     
-    $self->secret($app->secret);
     my $token_key = $options->{token_key_prefix}. "-token";
     
     $app->hook('around_dispatch' => sub {
-        (my $app, my $c) = @_;
+        (my $next, my $c) = @_;
         
         my @actions =
             ref $options->{action} ? @{$options->{action}} : $options->{action};
@@ -40,19 +37,19 @@ sub register {
         if ($req->method eq 'POST' && grep {$_ eq $req->url->path} @actions) {
             my $token = $c->param($token_key);
             $req->params->remove($token_key);
-            if (my $error = $self->validate_form($c, $token)) {
+            if (my $error = validate_form($c->tx->req->params, $token, $app->secret)) {
                 return $options->{blackhole}->($c, $error);
             }
         }
         
-        $app->();
+        $next->();
         
         if ($c->res->headers->content_type =~ qr{^text/html}) {
             my $dom = $c->res->dom;
             
             for my $action (@actions) {
                 $dom->find(qq{form[action="$action"][method="post"]})->each(sub {
-                    $self->inject_digest(shift, $token_key);
+                    inject_digest(shift, $token_key, $app->secret);
                 });
             }
             
@@ -62,7 +59,7 @@ sub register {
 }
 
 sub inject_digest {
-    my ($self, $form, $token_key) = @_;
+    my ($form, $token_key, $secret) = @_;
     my $digest = {};
     
     $form->find("*:not([disabled])[name]")->each(sub {
@@ -113,7 +110,7 @@ sub inject_digest {
         }
     }
     
-    my $signed = sign(digest_encode($digest), $self->secret);
+    my $signed = sign(digest_encode($digest), $secret);
     
     $form->append_content(sprintf(<<"EOF", $token_key, xml_escape $signed));
 <div style="display:none">
@@ -123,13 +120,13 @@ EOF
 }
 
 sub validate_form {
-    my ($self, $c, $token) = @_;
+    my ($params, $token, $secret) = @_;
 
     if (! $token) {
         return 'Token is not found';
     }
     
-    my $unsigned = unsign($token, $self->secret);
+    my $unsigned = unsign($token, $secret);
     
     if (! $unsigned) {
         return 'Token has been tampered';
@@ -137,40 +134,40 @@ sub validate_form {
     
     my $digest = digest_decode($unsigned);
     
-    for my $name ($c->param) {
+    for my $name ($params->param) {
         if (! $digest->{$name}) {
             return "Field $name is injected";
         }
     }
     for my $name (keys %{$digest}) {
-        if (! grep {$_ eq $name} $c->param) {
+        if (! grep {$_ eq $name} $params->param) {
             if (! $digest->{$name}->{$DIGEST_KEY_ALLOW_NULL}) {
                 return "Field $name is not given";
             }
         }
         if (my $allowed = $digest->{$name}->{$DIGEST_KEY_OPTIONS}) {
-            for my $given ($c->param($name)) {
+            for my $given ($params->param($name)) {
                 if (defined $given && ! grep {$_ eq $given} @$allowed) {
                     return "Field $name has been tampered";
                 }
             }
         }
         if (exists $digest->{$name}->{$DIGEST_KEY_MAXLENGTH}) {
-            for my $given ($c->param($name)) {
+            for my $given ($params->param($name)) {
                 if (length($given) > $digest->{$name}->{$DIGEST_KEY_MAXLENGTH}) {
                     return "Field $name is too long";
                 }
             }
         }
         if (defined $digest->{$name}->{$DIGEST_KEY_REQUIRED}) {
-            for my $given ($c->param($name)) {
+            for my $given ($params->param($name)) {
                 if (! $given || length($given) == 0) {
                     return "Field $name cannot be empty";
                 }
             }
         }
         if (my $pattern = $digest->{$name}->{$DIGEST_KEY_PATTERN}) {
-            for my $given ($c->param($name)) {
+            for my $given ($params->param($name)) {
                 if ($given !~ /\A$pattern\Z/) {
                     return "Field $name not match pattern";
                 }
@@ -178,18 +175,18 @@ sub validate_form {
         }
         if ($digest->{$name}->{$DIGEST_KEY_TYPE} &&
                             $digest->{$name}->{$DIGEST_KEY_TYPE} eq 'number') {
-            for my $given ($c->param($name)) {
+            for my $given ($params->param($name)) {
                 if ($given !~ /\A[\d\+\-\.]+\Z/) {
                     return "Field $name not match pattern";
                 }
                 if (my $min = $digest->{$name}->{$DIGEST_KEY_MIN}) {
-                    my $given = scalar $c->param($name);
+                    my $given = scalar $params->param($name);
                     if ($given < $min) {
                         return "Field $name too low";
                     }
                 }
                 if (my $max = $digest->{$name}->{$DIGEST_KEY_MAX}) {
-                    my $given = scalar $c->param($name);
+                    my $given = scalar $params->param($name);
                     if ($given > $max) {
                         return "Field $name too great";
                     }
@@ -272,6 +269,23 @@ The plugin detects following error for now.
 =item Values against maxlength attributes.
 
 =back
+
+=head2 CLASS METHODS
+
+=head3 inject_digest
+
+Generates a digest string of form structure and inject into itself.
+
+    $dom->find(qq{form[action="$action"][method="post"]})->each(sub {
+        my $form = shift;
+        inject_digest($form, $token_key, $app->secret);
+    });
+
+=head3 validate_form
+
+Validates given form data by given digest.
+
+    my $error = validate_form($c->tx->req->params, $digest, $app->secret);
 
 =head2 OPTIONS
 
